c447259bfa69e6499009160f6762d9c8b46286ca
PRANEET <127319827+Nani0778@users.noreply.github.com>
2024-02-09 00:04:27 +0530

    info.py

--- info.py
+++ info.py
@@ -68,8 +68,8 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
          # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'FlashshortX.in')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '223f5abd2e9815b52c4c0de6b32ef76610dd9a61')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'mdiskpro.com')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'xek:Al0Ujtd+YwJwr3Ktk/GZlw==')
 
 
      # Auto Delete For Group Message (Self Delete) #
